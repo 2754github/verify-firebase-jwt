691286feb3a4b628afa522d52cc2db39906b9fea
2754github <fkesys@gmail.com>
2021-03-18 22:44:57 +0900

    bundle gem _ -t

---
+++ lib/verify/firebase/jwt.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "jwt/version"
+
+module Verify
+  module Firebase
+    module Jwt
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/verify/firebase/jwt_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Verify::Firebase::Jwt do
+  it "has a version number" do
+    expect(Verify::Firebase::Jwt::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
